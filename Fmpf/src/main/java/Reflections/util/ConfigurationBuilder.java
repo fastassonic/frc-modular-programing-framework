1dca4fd5159562032c9db78ca55659cdd549e068
fastassonic <gabcall04@gmail.com>
2021-12-09 21:45:07 -0600

    Build command based boiler plate and started trying to get the local directory the code is being run from in order to be able to load modules

---
+++ Fmpf/src/main/java/Reflections/util/ConfigurationBuilder.java
@@ -0,0 +1,5 @@
+package Reflections.util;
+
+public class ConfigurationBuilder {
+
+}
---
+++ Fmpf/src/main/java/fmpf/fmpfstartup.java
@@ -0,0 +1,16 @@
+package fmpf;
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.reflections.Reflections;
+public class fmpfstartup {
+     // final File m_rawFile = new File("");
+    
+    public void fmpf(){
+        System.out.println("the direcotry is " + System.getProperty("user.dir"));
+        
+    };
+    
+};
+
+
